17ef2eb8c042561ac5251543d91ab96b017b3889
Alina Naukhatska <naukhatska.alina@gmail.com>
2024-09-09 17:50:56 +0300

    2.2

--- js/lektion2.1.js
+++ js/lektion2.1.js
@@ -26,7 +26,9 @@ const userLower = user.toLowerCase();
 
 // console.log(tytyty);
 
-//ЦИКЛ for 
+//!--------------------ЦИКЛЫ------------------------------------
+
+//!ЦИКЛ for 
 // циклом for для змшнной i (интератора)
 // начиная от 0 (=0) до 10 (<10) на каждом шаге додавляем 1 (+1)
 // console.log("before");
@@ -39,7 +41,119 @@ const userLower = user.toLowerCase();
 
 
 //! зависнет бразер, если цикл бесконечный
-for (let i = 10; i >= 0; i+=1) {
+// (10 > 0, i = 10)
+// (11 > 0, i = 12) и т.д. до бесконечности...
+// for (let i = 10; i >= 0; i+=1) {
 
-console.log(i);
+// console.log(i);
+// }
+
+//* Вывести каждую букву по одной в консоль!!!
+// const lalala = "world";
+
+// for(let i = 0; i < lalala.length; i+=1) {
+    
+//     //если надо прервать на 3м символе
+//     if (i === 3) {
+//         break;
+//     }
+//     console.log(lalala[i]);
+// }
+//* Инкремент та декремент (префиксный та постфиксный)
+// let a = 10;
+// const b = --a;
+
+// console.log("a", a);
+// console.log("b",b);
+
+//! ЦИКЛ while
+
+// let counter = 0;
+// while(counter < 5) {
+//     console.log(counter);
+//     counter +=1;
+// }
+
+//!ЦИКЛ do while (навить якщо умова хибна он отработает 1 раз)
+// let counter = 20;
+// do {
+//     console.log(counter);
+//     counter++;
+// }
+// while(counter <10);
+
+//*Задача: Напишить цикл for, який виводить в консоль браузера числа за зростанням от а до b
+//* Але тильки якщо число кратне 5
+
+// const a = 20;
+// const b = 100;
+
+// for(let i = a; i <= b; i++) {
+
+//     //если остаток от деления на 5 = 0, т.е если ! (НЕ) 0(false)
+//     if(!(i % 5)) {
+//         console.log(i);
+//     }
+// }
+
+//2ой вариант записиconst a = 20;
+// const a = 20;
+// const b = 100;
+
+// for(let i = a; i <= b; i++) {
+// //если отаток от деления i на 5 true (сурово равен нолю)
+//     if(i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
+
+//*Задача: Напиши скрипт, который подсчитывает сумму всех ПАРНЫХ чисел, 
+//*которые входят в диапазон чисел от min до max
+
+// const min = 0;
+// const max = 5;
+
+// let total = 0;
+
+// for(let i = min; i <= max; i++) {
+//     if(i % 2 === 0) {
+//         total += i;
+//     }
+
+// }
+// console.log(total);
+
+//НЕ ПАРНЫЕ откинуть (мозголомка)
+// const min = 0;
+// const max = 5;
+
+// let total = 0;
+
+// for(let i = min; i <= max; i++) {
+//     if(i % 2 !== 0) {
+//         console.log(i);
+//         continue;
+       
+//     }
+//     //  total += i;
+//     // console.log("after", i);
+    
+// }
+// console.log(total);
+
+
+//*Задача: Перевырити чи рядок меньше або доривнюэ длине maxLength (максимальное значение)
+// Если меньше или равен -> возвращает весь рядок
+// Если больше -> обрезает и добавляет ...
+
+function formatMessage(message, maxLength) {
+    if (message.length <= maxLength) {
+        return message;
+        
+    } else {
+        return message.slice(0, maxLength) + "...";
+        
+    }
 }
+
+console.log(formatMessage("Lorem ipsum dolor sit amet", 15));
